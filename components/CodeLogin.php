49c9ad054bceffa2d3f2f69d064e17098310fb59
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2016-02-22 22:07:33 +0100

    Fix file format

--- components/CodeLogin.php
+++ components/CodeLogin.php
@@ -16,23 +16,23 @@ use VojtaSvoboda\CodeLogin\Repositories\UserRepository;
 
 class CodeLogin extends ComponentBase
 {
-	public function componentDetails()
-	{
-		return [
-			'name'        => 'vojtasvoboda.codelogin::lang.logincomponent.name',
-			'description' => 'vojtasvoboda.codelogin::lang.logincomponent.description'
-		];
-	}
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'vojtasvoboda.codelogin::lang.logincomponent.name',
+            'description' => 'vojtasvoboda.codelogin::lang.logincomponent.description'
+        ];
+    }
 
-	public function defineProperties()
-	{
-		return [
-			'redirect' => [
-				'title'       => 'vojtasvoboda.codelogin::lang.logincomponent.redirect.title',
-				'description' => 'vojtasvoboda.codelogin::lang.logincomponent.redirect.description',
-				'type'        => 'dropdown',
-				'default'     => ''
-			],
+    public function defineProperties()
+    {
+        return [
+            'redirect' => [
+                'title'       => 'vojtasvoboda.codelogin::lang.logincomponent.redirect.title',
+                'description' => 'vojtasvoboda.codelogin::lang.logincomponent.redirect.description',
+                'type'        => 'dropdown',
+                'default'     => ''
+            ],
             'visible' => [
                 'title'       => 'vojtasvoboda.codelogin::lang.logincomponent.visible.title',
                 'description' => 'vojtasvoboda.codelogin::lang.logincomponent.visible.description',
@@ -45,81 +45,81 @@ class CodeLogin extends ComponentBase
                 'type'        => 'string',
                 'default'     => 'enter'
             ]
-		];
-	}
+        ];
+    }
 
-	public function getRedirectOptions()
-	{
+    public function getRedirectOptions()
+    {
         $default = [
             '' => '- none -'
         ];
         $pages = Page::sortBy('baseFileName')->lists('baseFileName', 'baseFileName');
 
-		return $default + $pages;
-	}
+        return $default + $pages;
+    }
 
-	/**
-	 * Executed when this component is bound to a page or layout.
-	 */
-	public function onRun()
-	{
-		$this->page['visible'] = $this->property('visible');
+    /**
+     * Executed when this component is bound to a page or layout.
+     */
+    public function onRun()
+    {
+        $this->page['visible'] = $this->property('visible');
         $this->page['button'] = $this->property('button');
-	}
+    }
 
-	/**
-	 * Sign in the user
-	 */
-	public function onCodesignin()
-	{
-		/*
-		 * Validate input
-		 */
-		$data = post();
+    /**
+     * Sign in the user
+     */
+    public function onCodesignin()
+    {
+        /*
+         * Validate input
+         */
+        $data = post();
 
-		$rules = [];
-		$rules['code'] = 'required|min:2';
+        $rules = [];
+        $rules['code'] = 'required|min:2';
 
-		$messages = [];
-		$messages['required'] = trans('vojtasvoboda.codelogin::lang.form.required');
+        $messages = [];
+        $messages['required'] = trans('vojtasvoboda.codelogin::lang.form.required');
 
-		$validation = Validator::make($data, $rules, $messages);
-		if ($validation->fails()) {
-			throw new ValidationException($validation);
-		}
+        $validation = Validator::make($data, $rules, $messages);
+        if ($validation->fails()) {
+            throw new ValidationException($validation);
+        }
 
-		/*
-		 * Find user by password
-		 */
-		$users = new UserRepository();
-		$userToLog = $users->getUserByPassword(array_get($data, 'code'));
-		if ($userToLog === null) {
-			$exception = new ValidationException([trans('vojtasvoboda.codelogin::lang.form.wrong_code')]);
-			throw $exception;
-		}
+        /*
+         * Find user by password
+         */
+        $users = new UserRepository();
+        $userToLog = $users->getUserByPassword(array_get($data, 'code'));
+        if ($userToLog === null) {
+            $exception = new ValidationException([trans('vojtasvoboda.codelogin::lang.form.wrong_code')]);
+            throw $exception;
+        }
 
-		/*
-		 * Authenticate user
-		 */
-		$remember = true;
-		$user = Auth::authenticate([
-			'login' => $userToLog->email,
-			'password' => array_get($data, 'code')
-		], $remember);
+        /*
+         * Authenticate user
+         */
+        $remember = true;
+        $user = Auth::authenticate([
+            'login' => $userToLog->email,
+            'password' => array_get($data, 'code')
+        ], $remember);
 
-		/*
-		 * After login event
-		 */
-		Event::fire('vojtasvoboda.codelogin.afterlogin', [$user]);
+        /*
+         * After login event
+         */
+        Event::fire('vojtasvoboda.codelogin.afterlogin', [$user]);
 
-		/*
-		 * Redirect to the intended page after successful sign in
-		 */
-		$redirectUrl = $this->pageUrl($this->property('redirect'));
+        /*
+         * Redirect to the intended page after successful sign in
+         */
+        $redirectUrl = $this->pageUrl($this->property('redirect'));
 
-		if ($redirectUrl = post('redirect', $redirectUrl)) {
-			return Redirect::intended($redirectUrl);
-		}
-	}
+        if ($redirectUrl = post('redirect', $redirectUrl)) {
+            return Redirect::intended($redirectUrl);
+        }
+    }
 
 }
